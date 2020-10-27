d193a00cf247442fe293a2b00ab9ab7bb3c7d7ac
cubanallstars <danangdevils@gmail.com>
2020-10-27 12:20:06 +0000

    Changed message

--- GenerateNonsense/index.js
+++ GenerateNonsense/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
     let now = new Date();
     context.res = {
-        body: "The date & time now is: " + now.toISOString() + "T" + now.toTimeString()
+        body: "This is the date and time: " + now.toISOString() + "T" + now.toTimeString()
     };
 };
